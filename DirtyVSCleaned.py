f21cc0fa981b246de7114aa7d797d29a3ed8f203
i.ilyasov <igorilyasov94@gmail.com>
2021-02-26 03:25:05 +0300

    Dataset preparation & loading

--- DirtyVSCleaned.py
+++ DirtyVSCleaned.py
@@ -1,1 +1,32 @@
+import PrepareDataset as dataset
 
+import torch
+import torch.utils.data
+import numpy as np
+import torchvision
+import matplotlib.pyplot as plt
+import time
+import copy
+
+from torchvision import transforms, models
+
+train_transforms = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+val_transforms = transforms.Compose([
+    transforms.Resize(224, 224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+train_dataset = torchvision.datasets.ImageFolder(dataset.train_dir, train_transforms)
+val_dataset = torchvision.datasets.ImageFolder(dataset.val_dir, val_transforms)
+
+batch_size = 8
+
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=batch_size)
+val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=batch_size)
